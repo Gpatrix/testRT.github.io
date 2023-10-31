f835973d5b1c43349d8942e807288fed7ca76af1
Gpatrix <gpatrix.fr@gmail.com>
2023-10-31 18:06:18 +0100

    1.0

---
+++ asset/js/swiper-script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        initialSlide: 2, // commence a 0
+        allowTouchMove: false, // Désactiver la possibilité de faire glisser les slides
+    });
+
+    // Écoutez les clics sur les liens de navigation
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const slideIndex = this.getAttribute('data-slide-index');
+            mySwiper.slideToLoop(slideIndex);
+        });
+    });
+    const subnavLinks = document.querySelectorAll(".in-lk");
+    subnavLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const slideIndex = this.getAttribute('data-slide-index');
+            mySwiper.slideToLoop(slideIndex);
+        });
+    });
+});
+
+const egg = document.getElementById("egg");
+let beat = new Audio('asset/js/audi.m4a');
+const eggButton = document.getElementById("Button");
+eggButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    beat.play();
+});
